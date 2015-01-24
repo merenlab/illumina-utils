a05bd8d080b5cc9e4cf181bc3434c0010516dbef
A. Murat Eren <a.murat.eren@gmail.com>
2015-01-24 18:58:40 -0500

    a very simple test to see how demultiplexing done

---
+++ Unittests/demultiplexing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+C() {
+    echo -e "\033[0;30m\033[46m$1\033[0m"
+}
+
+INFO() { 
+    echo
+    C "###############################################################"
+    C "#"
+    C "# $1"
+    C "#"
+    C "###############################################################"
+    echo
+} 
+
+INFO "Creating the output directory ..."
+# change directory and clean the old mess if it exists
+cd demultiplexing
+rm -rf output
+mkdir output
+
+INFO "Running demultiplexing ..."
+iu-demultiplex -s barcode_to_sample.txt --r1 r1.fastq --r2 r2.fastq --index index.fastq -o output/
+
+INFO "Done."
