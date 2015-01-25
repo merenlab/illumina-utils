fc91394ffb4663aa3f807f80099cbbbb93ef2007
A. Murat Eren <a.murat.eren@gmail.com>
2015-01-24 21:29:14 -0500

    updating with the basic usage of ui-gen-configs...

--- examples/demultiplexing.sh
+++ examples/demultiplexing.sh
@@ -23,4 +23,8 @@ mkdir output
 INFO "Running demultiplexing ..."
 iu-demultiplex -s barcode_to_sample.txt --r1 r1.fastq --r2 r2.fastq --index index.fastq -o output/
 
+INFO "Generating config files for each demultiplexed sample"
+
+iu-gen-configs output/00_DEMULTIPLEXING_REPORT
+
 INFO "Done."
