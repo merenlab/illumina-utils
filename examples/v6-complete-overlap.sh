fa7dbe8c387bf2c2cad85f09d581779d4823915b
A. Murat Eren <a.murat.eren@gmail.com>
2015-01-27 19:16:47 -0500

    new binaries

--- examples/v6-complete-overlap.sh
+++ examples/v6-complete-overlap.sh
@@ -9,13 +9,13 @@ rm -rf output
 mkdir output
 
 INFO "Generating the config file from sample descripton"
-../../scripts/iu-gen-configs sample-description.txt -o output --r1-prefix 'A[AC][CG]CG[AC].[AG]AACCT[CT]A.C' --r2-prefix '^....C[AG][AG]CCATGCA.CACCT'
+iu-gen-configs sample-description.txt -o output --r1-prefix 'A[AC][CG]CG[AC].[AG]AACCT[CT]A.C' --r2-prefix '^....C[AG][AG]CCATGCA.CACCT'
 
 INFO "Running complete overlap analysis for V6 ..."
-merge-illumina-pairs output/E.coli.ini --marker-gene-stringent --retain-only-overlap
+iu-merge-pairs output/E.coli.ini --marker-gene-stringent --retain-only-overlap
 
 INFO "Retaining only reads with 0 mismatches"
-filter-merged-reads output/E.coli_MERGED --max-mismatches 0
+iu-filter-merged-reads output/E.coli_MERGED --max-mismatches 0
 
 
 INFO "Done."
