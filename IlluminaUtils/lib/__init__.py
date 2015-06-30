0aaa9818f46f0507085bf887415179df90f7fbca
A. Murat Eren <a.murat.eren@gmail.com>
2015-06-30 09:48:39 -0400

    a bit better way to access to the version info

--- IlluminaUtils/lib/__init__.py
+++ IlluminaUtils/lib/__init__.py
@@ -1,2 +1,11 @@
+import os
 import pkg_resources
-__version__ = pkg_resources.require("illumina-utils")[0].version
+
+try:
+    __version__ = pkg_resources.require("illumina-utils")[0].version
+except:
+    # maybe it is not installed but being run from the codebase dir?
+    try:
+        __version__ = open(os.path.normpath(os.path.dirname(os.path.abspath(__file__))) + '/../../VERSION').read().strip()
+    except:
+        __version__ = 'unknown'
