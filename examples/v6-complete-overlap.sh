deaf1459b21e2d6c0f995d7066a30cc6f6ea6352
A. Murat Eren <a.murat.eren@gmail.com>
2016-05-10 12:06:38 -0500

    new workflow

--- examples/v6-complete-overlap.sh
+++ examples/v6-complete-overlap.sh
@@ -9,13 +9,12 @@ rm -rf output
 mkdir output
 
 INFO "Generating the config file from sample descripton"
-iu-gen-configs sample-description.txt -o output --r1-prefix 'A[AC][CG]CG[AC].[AG]AACCT[CT]A.C' --r2-prefix '^....C[AG][AG]CCATGCA.CACCT'
+iu-gen-configs sample-description.txt -o output 
 
-INFO "Running complete overlap analysis for V6 ..."
-iu-merge-pairs output/E.coli.ini --marker-gene-stringent --retain-only-overlap
+INFO "Running complete overlap analysis with 0 mismatches ..."
+iu-merge-pairs output/E.coli.ini --marker-gene-stringent --retain-only-overlap --max-num-mismatches 0
 
-INFO "Retaining only reads with 0 mismatches"
-iu-filter-merged-reads output/E.coli_MERGED --max-mismatches 0
+INFO "Trimming V6 primers from merged reads ..."
+iu-trim-V6-primers output/E.coli_MERGED
 
-
-INFO "Done."
+INFO "Done. Please see v6-complete-overlap/output/E.coli_MERGED_V6_PRIMERS_REMOVED"
